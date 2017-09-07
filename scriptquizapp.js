b8556c839c5c6005c0314c53c7d29c01567bddbf
derek <derekcstein@gmail.com>
2017-09-07 09:21:51 -0700

    Finished Quiz App\

---
+++ scriptquizapp.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#showAnswer1").click(function() {
+		$("#Answer1").css({"backgroundColor":"white","color":"black"})
+	});
+	$("#showAnswer2").click(function() {
+		$("#Answer2").css({"backgroundColor":"white","color":"black"})
+	});
+	$("#showAnswer3").click(function() {
+		$("#Answer3").css({"backgroundColor":"white","color":"black"})
+	});
+		$("#showAnswer4").click(function() {
+		$("#Answer4").css({"backgroundColor":"white","color":"black"})
+	});
+		$("#showAnswer5").click(function() {
+		$("#Answer5").css({"backgroundColor":"white","color":"black"})
+	});
+});
